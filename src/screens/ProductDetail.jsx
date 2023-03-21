c5eb788540d67dd7d2b4492e0f5beda2d9c2b2e9
Alexander Garzo <87275990+garzo94@users.noreply.github.com>
2023-03-21 17:15:07 -0600

    Refactoring code

--- src/screens/ProductDetail.jsx
+++ src/screens/ProductDetail.jsx
@@ -1,43 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { SafeAreaView, Text } from "react-native";
+import React from "react";
+import { SafeAreaView, Text, ScrollView } from "react-native";
 import ProductDetailCard from "../components/ProductDetailCard";
-import { COLORS } from "../styles/colors";
-import Reviews from "../components/DetailScreen/Reviews";
-import { productDetailStyle } from "../styles/ProductDetail/ProductDetailStyle";
-import { Feather } from "@expo/vector-icons";
-import {
-  Collapse,
-  CollapseHeader,
-  CollapseBody,
-} from "accordion-collapse-react-native";
-export default function ProductDetail() {
-  const [isActive, setIsActive] = useState(false);
+import { productDetailCard as style } from "../styles/ProductDetail/ProductDetailCard";
 
+export default function ProductDetail() {
   return (
-    <SafeAreaView style={{ backgroundColor: COLORS.PURPLE, height: "100%" }}>
-      <ProductDetailCard />
-      <Collapse isExpanded={isActive} onToggle={() => setIsActive(!isActive)}>
-        <CollapseHeader style={productDetailStyle.collapseHeader}>
-          <Text
-            style={{
-              color: "#fff",
-              fontSize: 22,
-              marginLeft: 20,
-            }}
-          >
-            Reviews
-          </Text>
-          <Feather
-            name={isActive ? "chevron-up" : "chevron-down"}
-            size={24}
-            // color={COLORS.DARK_PURPLE}
-            style={{ marginRight: 20, color: "white" }}
-          />
-        </CollapseHeader>
-        <CollapseBody>
-          <Reviews />
-        </CollapseBody>
-      </Collapse>
+    <SafeAreaView style={style.containerStyle}>
+      <ScrollView>
+        <ProductDetailCard />
+      </ScrollView>
     </SafeAreaView>
   );
 }
