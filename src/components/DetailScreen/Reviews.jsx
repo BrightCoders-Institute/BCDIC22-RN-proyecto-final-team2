7f6e9bbfe591296dc3047541686662e278f15fce
Alexander Garzo <87275990+garzo94@users.noreply.github.com>
2023-03-21 17:03:18 -0600

    real data integration

--- src/components/DetailScreen/Reviews.jsx
+++ src/components/DetailScreen/Reviews.jsx
@@ -1,27 +1,23 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { COLORS } from "../../styles/colors";
 import { AirbnbRating } from "react-native-ratings";
 import { reviewStyle } from "../../styles/ProductDetail/ReviewsStyle";
 
-export default function Reviews() {
+export default function Reviews({ user, rating, review, date }) {
   return (
     <View style={reviewStyle.reviewContainer}>
       <View style={reviewStyle.ratingContainer}>
-        <Text style={reviewStyle.usernameStyle}>Username</Text>
+        <Text style={reviewStyle.usernameStyle}>{user}</Text>
         <AirbnbRating
           showRating={false}
           size={15}
-          defaultRating={4}
+          defaultRating={rating}
           isDisabled={true}
           starContainerStyle={{ marginLeft: 5 }}
         />
       </View>
-      <Text style={reviewStyle.description}>
-        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet. Porro
-        quisquam est qui dolorem ipsum
-      </Text>
-      <Text style={reviewStyle.date}>Feb 15, 2023</Text>
+      <Text style={reviewStyle.description}>{review}</Text>
+      <Text style={reviewStyle.date}>{date}</Text>
     </View>
   );
 }
