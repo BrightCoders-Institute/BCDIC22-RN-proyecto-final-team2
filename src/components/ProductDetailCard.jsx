cc75dd661161c152f4b5b198f7034ffa0aa4441c
Alexander Garzo <87275990+garzo94@users.noreply.github.com>
2023-03-21 17:11:54 -0600

    refactoring and real data integration

--- src/components/ProductDetailCard.jsx
+++ src/components/ProductDetailCard.jsx
@@ -1,89 +1,165 @@
-import { Text, View, TouchableOpacity, Image, Pressable } from "react-native";
-import React, { PureComponent } from "react";
+import {
+  Text,
+  View,
+  Image,
+  Pressable,
+  Alert,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import React, { useState, useEffect } from "react";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { AirbnbRating } from "react-native-ratings";
 import { COLORS } from "../styles/colors";
-import { containers } from "../styles/HomeScreen/Components_ProductCard";
 import { elements } from "../styles/HomeScreen/Components_ProductCard";
-import { productDetailStyle } from "../styles/ProductDetail/ProductDetailCard";
-export default class ProductDetailCard extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isFavorite: false,
-    };
+import Reviews from "../components/DetailScreen/Reviews";
+import { productDetailCard } from "../styles/ProductDetail/ProductDetailCard";
+import { Feather } from "@expo/vector-icons";
+import { productDetailStyle } from "../styles/ProductDetail/ProductDetailStyle";
+
+import {
+  Collapse,
+  CollapseHeader,
+  CollapseBody,
+} from "accordion-collapse-react-native";
+const ProductDetailCard = () => {
+  const [isActive, setIsActive] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [details, setDetails] = useState("");
+
+  async function fetchProductDetails() {
+    try {
+      const response = await fetch(
+        "https://findgure.up.railway.app/api/product/detail/2/"
+      );
+      const data = await response.json();
+      setDetails(data);
+      return data;
+    } catch (error) {
+      Alert.alert(error);
+    }
   }
 
-  render() {
-    return (
-      <View style={productDetailStyle.cardContainer}>
-        <Image
-          style={productDetailStyle.imageProduct}
-          source={{
-            uri: "https://findgure.s3.amazonaws.com/products/Deadpool.png",
-          }}
-        />
+  useEffect(() => {
+    fetchProductDetails();
+  }, []);
 
-        <View>
-          <Pressable
-            onPress={() => {
-              this.setState({ isFavorite: !this.state.isFavorite });
+  return (
+    <>
+      {/* card details */}
+      {details !== "" ? (
+        <View style={productDetailCard.cardContainer}>
+          <Image
+            style={productDetailCard.imageProduct}
+            source={{
+              uri: details.image,
             }}
-          >
-            <FontAwesome
-              name={this.state.isFavorite ? "heart" : "heart-o"}
-              size={24}
-              color={COLORS.DARK_PURPLE}
-              style={productDetailStyle.heartIcon}
-            />
-          </Pressable>
-
-          <Text style={elements.productTitle}>
-            {"product name".length > 10
-              ? "Product name".substring(0, 7) + "..."
-              : "Product name"}
-          </Text>
-          <Text style={elements.textFranchise}>
-            Franchise:{" "}
-            <Text style={elements.productFranchise}>
-              {"franchise name".length > 15
-                ? "Franchise name".substring(0, 10) + "..."
-                : "Franchise name"}
-            </Text>
-          </Text>
-          <Text style={elements.textCategory}>
-            Category: <Text style={elements.productCategory}>My category</Text>
-          </Text>
-          <Text
-            numberOfLines={10}
-            style={productDetailStyle.descriptionProduct}
-          >
-            Neque porro quisquam est qui dolorem ipsum quia dolor sit
-            amet.dolorem ipsum quia dolor sit amet
-          </Text>
-          <AirbnbRating
-            showRating={false}
-            size={15}
-            defaultRating={5}
-            isDisabled={true}
-            starContainerStyle={productDetailStyle.starContainer}
           />
-          <Text style={productDetailStyle.priceProduct}>$ {45 + ".00"}</Text>
-          <View></View>
-          <Pressable onButtonPress style={productDetailStyle.buttonBuy}>
-            <Text style={elements.textButtonBuy}>Buy Now</Text>
-          </Pressable>
-          <Pressable onButtonPress style={containers.buttonAddToCart}>
-            <Text style={elements.textButtonAddToCart}>Add to cart</Text>
-            <Ionicons
-              name="cart-outline"
-              size={16}
-              color={COLORS.DARK_PURPLE}
-              style={elements.cartIcon}
+
+          <View>
+            <Pressable onPress={() => setIsFavorite(!isFavorite)}>
+              <FontAwesome
+                name={isFavorite ? "heart" : "heart-o"}
+                size={24}
+                color={COLORS.DARK_PURPLE}
+                style={productDetailCard.heartIcon}
+              />
+            </Pressable>
+
+            <Text style={elements.productTitle}>
+              {details.name.length > 10
+                ? details.name.length.substring(0, 7) + "..."
+                : details.name}
+            </Text>
+            <Text style={elements.textFranchise}>
+              Franchise:{" "}
+              <Text style={elements.productFranchise}>
+                {details.franchise.name.length > 15
+                  ? details.franchise.name.substring(0, 10) + "..."
+                  : details.franchise.name}
+              </Text>
+            </Text>
+            <Text style={elements.textCategory}>
+              Category:{" "}
+              <Text style={elements.productCategory}>
+                {details.franchise.category}
+              </Text>
+            </Text>
+            <Text
+              numberOfLines={10}
+              style={productDetailCard.descriptionProduct}
+            >
+              {details.description}
+            </Text>
+            <AirbnbRating
+              showRating={false}
+              size={15}
+              defaultRating={details.rating}
+              isDisabled={true}
+              starContainerStyle={productDetailCard.starContainer}
             />
-          </Pressable>
+            <Text style={productDetailCard.priceProduct}>
+              $ {details.price + ".00"}
+            </Text>
+            <Pressable style={productDetailCard.buttonBuy}>
+              <Text style={elements.textButtonBuy}>Buy Now</Text>
+            </Pressable>
+            <Pressable style={productDetailCard.buttonAddToCart}>
+              <Text style={elements.textButtonAddToCart}>Add to cart</Text>
+              <Ionicons
+                name="cart-outline"
+                size={16}
+                color={COLORS.DARK_PURPLE}
+                style={elements.cartIcon}
+              />
+            </Pressable>
+          </View>
         </View>
-      </View>
-    );
-  }
-}
+      ) : (
+        <Text>Loading...</Text>
+      )}
+
+      {/* Reviews */}
+      {details && (
+        <Collapse isExpanded={isActive} onToggle={() => setIsActive(!isActive)}>
+          <CollapseHeader style={productDetailStyle.collapseHeader}>
+            <Text style={productDetailStyle.textReview}>Reviews</Text>
+            <Feather
+              name={isActive ? "chevron-up" : "chevron-down"}
+              size={24}
+              style={productDetailStyle.arrowIcon}
+            />
+          </CollapseHeader>
+          <CollapseBody>
+            <ScrollView>
+              <FlatList
+                data={details.reviews}
+                keyExtractor={(review) => review.user}
+                renderItem={({ item }) => {
+                  const date = new Date(item.date_created);
+                  const day = date.getDate();
+                  const year = date.getFullYear();
+                  const month = date
+                    .toLocaleString("default", { month: "long" })
+                    .split(" ")[1];
+                  const formatDate = `${day} ${month}, ${year}`;
+                  return (
+                    <Reviews
+                      key={item.user}
+                      user={item.user}
+                      rating={item.rating}
+                      review={item.review}
+                      date={formatDate}
+                    />
+                  );
+                }}
+              />
+            </ScrollView>
+          </CollapseBody>
+        </Collapse>
+      )}
+    </>
+  );
+};
+
+export default ProductDetailCard;
