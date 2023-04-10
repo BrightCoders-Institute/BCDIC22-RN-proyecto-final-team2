f9ca2a8b29754db571c6f009873793c66d7f7d20
Enrique Mendoza <henrikhwolf@gmail.com>
2023-04-10 14:13:28 -0600

    Indentation fixed

--- src/components/WishConfirm.jsx
+++ src/components/WishConfirm.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 import { ConfirmedWish } from '../styles/WishlistScreen/Confirmed';
 
 export default class WishConfirm extends Component{
-    render() {
-        return (
-        <View style={ConfirmedWish.container}>
-        <Text style={ConfirmedWish.message}> Succesfully <Text style={ConfirmedWish.added}>added</Text> to your wishlist. </Text>
-        </View>
-        )
-    }
+  render() {
+    return (
+      <View style={ConfirmedWish.container}>
+      <Text style={ConfirmedWish.message}> Succesfully <Text style={ConfirmedWish.added}>added</Text> to your wishlist. </Text>
+      </View>
+    )
+  }
 }
--- src/components/WishRmConfirm.jsx
+++ src/components/WishRmConfirm.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 import { ConfirmedWish } from '../styles/WishlistScreen/Confirmed';
 
 export default class WishRmConfirm extends Component{
-    render() {
-        return (
-        <View style={ConfirmedWish.container}>
-        <Text style={ConfirmedWish.message}> Succesfully <Text style={ConfirmedWish.removed}>removed</Text> from your wishlist. </Text>
-        </View>
-        )
-    }
+  render() {
+    return (
+      <View style={ConfirmedWish.container}>
+      <Text style={ConfirmedWish.message}> Succesfully <Text style={ConfirmedWish.removed}>removed</Text> from your wishlist. </Text>
+      </View>
+    )
+  }
 }
